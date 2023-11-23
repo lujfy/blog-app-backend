fdaf10dc89a6856b9a30a8fdb6eabb58ab8b9e57
lujfy <lujfy50@gmail.com>
2023-11-23 22:37:50 +0700

    add new node

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express()
 
 app.use(cors(
   {credentials:true,
-    origin:['http://localhost:5173' , 'https://blogapp-pvsw.onrender.com']
+    origin:['http://localhost:4173' , 'http://localhost:5173' , 'https://blogapp-pvsw.onrender.com']
   }));
 
 app.use(express.json())
